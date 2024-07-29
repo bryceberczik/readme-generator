3feed0f5e9cf88d8436f61209e78aa0627e8d431
bryceberczik <berczikbryce@gmail.com>
2024-07-29 15:14:20 -0500

    Made changes to the README file.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -80,7 +80,7 @@ ${data.contribution}
 ${data.tests}
 
 ## Questions
-If you have any questions, you can reach me at [${data.email}](mailto:${data.email}). You can also find more of my work at [${data.github}](https://github.com/${data.github}).
+If you have any questions, you can reach me at [${data.email}](mailto:${data.email}). You can also find more of my work at [${data.github}](https://github.com/${data.github}) on Github.
 `;
 }
 
