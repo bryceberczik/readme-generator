9f5b3b0cf15ab126c5d981f389a98b9adf5f9c20
bryceberczik <berczikbryce@gmail.com>
2024-07-29 01:13:57 -0500

    Finished Module 7 Challenge: README Generator

---
+++ Develop/index.js
@@ -0,0 +1,73 @@
+// TODO: Include packages needed for this application
+
+import inquirer from 'inquirer';
+import fs from 'fs';
+import generateMarkdown from './utils/generateMarkdown.js';
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are the installation instructions?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide usage information:',
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'What are the contribution guidelines?',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'What are the test instructions?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Finally, choose a license for your project:',
+        choices: ['MIT', 'GPLv3', 'Apache 2.0', 'None'],
+      },
+  ];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+      console.log(err ? `Error writing file: ${err}` : 'README file has been generated successfully!');
+    });
+  }
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+      const data = generateMarkdown(answers);
+      writeToFile('README.md', data);
+    });
+  }
+
+// Function call to initialize app
+init();
